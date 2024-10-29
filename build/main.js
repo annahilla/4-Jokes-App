32e17df24466712b5813fc06ba945bebfa93a3e8
Anna <annahilla21@gmail.com>
2024-10-29 13:24:44 +0100

    Added voting joke functionality

--- build/main.js
+++ build/main.js
@@ -1,7 +1,11 @@
 "use strict";
 var jokeOutput = document.getElementById('jokeOutput');
 var btn = document.getElementById('btn');
+var voteBtns = document.querySelectorAll('.vote-joke-btn');
 btn.addEventListener('click', getJoke);
+voteBtns.forEach(function (button) { return button.addEventListener('click', function () { return voteJoke(currentJoke, button.id); }); });
+var currentJoke = '';
+var jokeVoted = false;
 var options = {
     headers: {
         'Accept': 'application/json'
@@ -11,7 +15,30 @@ function getJoke() {
     fetch('https://icanhazdadjoke.com/', options)
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        jokeOutput.innerHTML = data.joke;
+        currentJoke = data.joke;
+        jokeOutput.innerHTML = currentJoke;
+        jokeVoted = false;
     });
 }
 getJoke();
+var reportAcudits = [];
+function voteJoke(joke, btnId) {
+    var currentVote = {
+        joke: joke,
+        score: parseInt(btnId),
+        date: (new Date()).toISOString()
+    };
+    if (jokeVoted) {
+        reportAcudits.map(function (joke) {
+            if (joke.joke === currentVote.joke) {
+                joke.score = currentVote.score;
+                return;
+            }
+        });
+    }
+    else {
+        reportAcudits.push(currentVote);
+        jokeVoted = true;
+    }
+    console.log(reportAcudits);
+}
--- script/main.ts
+++ script/main.ts
@@ -1,7 +1,12 @@
 const jokeOutput= document.getElementById('jokeOutput') as HTMLElement;
 const btn = document.getElementById('btn') as HTMLElement;
+const voteBtns = document.querySelectorAll('.vote-joke-btn');
 
-btn.addEventListener('click', getJoke)
+btn.addEventListener('click', getJoke);
+voteBtns.forEach(button => button.addEventListener('click', () => voteJoke(currentJoke, button.id)));
+
+let currentJoke: string = '';
+let jokeVoted: boolean = false;
 
 const options = {
     headers: {
@@ -13,8 +18,40 @@ function getJoke() {
     fetch('https://icanhazdadjoke.com/', options)
         .then(response => response.json())
         .then(data => {
-            jokeOutput.innerHTML = data.joke;
+            currentJoke = data.joke;
+            jokeOutput.innerHTML = currentJoke;
+            jokeVoted = false;
         });
 }
 
 getJoke();
+
+interface ReportAcudit {
+    joke: string,
+    score: number,
+    date: string
+}
+
+const reportAcudits: ReportAcudit[] = [];
+
+function voteJoke(joke: string, btnId:string) {   
+    let currentVote: ReportAcudit = {
+        joke: joke,
+        score: parseInt(btnId),
+        date: (new Date()).toISOString()
+    }
+
+    if(jokeVoted) {
+        reportAcudits.map(joke => {
+            if(joke.joke === currentVote.joke) {
+                joke.score = currentVote.score;
+                return;
+            }
+        })
+    } else {
+        reportAcudits.push(currentVote);
+        jokeVoted = true;
+    }
+
+    console.log(reportAcudits);
+}
